8afd849e3668b7ea6d8bdcfb17c128edcbeafa8f
Luke San Antonio <lukesanantonio@gmail.com>
2015-10-03 09:52:37 -0400

    Comment out references to servos
    For now we don't have any, so this should prevent a crash.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TeleOp.java
@@ -107,8 +107,8 @@ public class K9TeleOp extends OpMode {
 		motorLeft = hardwareMap.dcMotor.get("motor_1");
 		motorLeft.setDirection(DcMotor.Direction.REVERSE);
 		
-		arm = hardwareMap.servo.get("servo_1");
-		claw = hardwareMap.servo.get("servo_6");
+		//arm = hardwareMap.servo.get("servo_1");
+		//claw = hardwareMap.servo.get("servo_6");
 
 		// assign the starting position of the wrist and claw
 		armPosition = 0.2;
@@ -179,8 +179,8 @@ public class K9TeleOp extends OpMode {
         clawPosition = Range.clip(clawPosition, CLAW_MIN_RANGE, CLAW_MAX_RANGE);
 
 		// write position values to the wrist and claw servo
-		arm.setPosition(armPosition);
-		claw.setPosition(clawPosition);
+		//arm.setPosition(armPosition);
+		//claw.setPosition(clawPosition);
 
 
 
