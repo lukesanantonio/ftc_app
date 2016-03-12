6b375b453f304d40fb6abf0eef1df83721290e69
Luke San Antonio <lukesanantonio@gmail.com>
2016-03-11 20:36:32 -0500

    Add camera test code

--- robotcameratest/src/main/java/net/redcrane/robotcameratest/CameraTestActivity.java
+++ robotcameratest/src/main/java/net/redcrane/robotcameratest/CameraTestActivity.java
@@ -1,6 +1,10 @@
 package net.redcrane.robotcameratest;
 
 import android.annotation.SuppressLint;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
@@ -15,7 +19,17 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.features2d.*;
+
+import org.opencv.core.Rect;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.core.Point;
+
+import org.opencv.imgproc.Imgproc;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -24,6 +38,13 @@ import org.opencv.core.Mat;
 public class CameraTestActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private CameraBridgeViewBase mOpenCvCameraView;
+
+    static final String TAG = "CameraTestActivity";
+
+    Mat hsvMat;
+    Mat redMat;
+    Mat blueMat;
+    Mat old;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,12 +98,76 @@ public class CameraTestActivity extends Activity implements CameraBridgeViewBase
     }
 
     public void onCameraViewStarted(int width, int height) {
+        hsvMat = new Mat();
+        redMat = new Mat();
+        blueMat = new Mat();
+        old = new Mat();
     }
 
     public void onCameraViewStopped() {
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        Imgproc.cvtColor(inputFrame.rgba(), hsvMat, Imgproc.COLOR_RGB2HSV);
+
+
+        // Average Red color
+        // H: 7 S: 55 V: 200
+        // Blue
+        // H: S: V:
+
+        //double[] vals = hsvMat.get(0,0);
+        //Log.i(TAG, "H: " + vals[0] + " S: " + vals[1] + " V: " + vals[2]);
+        //return hsvMat;
+
+        Core.inRange(hsvMat, new Scalar(0xa4, 0x14, 0xe0), new Scalar(0xc0, 0x50, 0xff), redMat);
+        //Core.bitwise_not(redMat, redMat);
+
+        // Do the red mat
+        // First remove small things
+        Imgproc.erode(redMat, redMat, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(10, 10)));
+        Imgproc.dilate(redMat, redMat, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(10, 10)));
+
+        // Blue now
+        //Core.inRange(hsvMat, new Scalar(0x20, 0x35, 0x80), new Scalar(0x90, 0x70, 0xff), blueMat);
+        //Core.bitwise_not(blueMat, blueMat);
+
+        // Do the red mat
+        // First remove small things
+        //Imgproc.erode(blueMat, blueMat, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(7, 7)));
+        //Imgproc.dilate(blueMat, blueMat, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(7, 7)));
+
+        // Accumulate the two images
+        //Imgproc.accumulate(redMat, blueMat);
+
+        //Point shift = Imgproc.phaseCorrelate(redMat, old);
+        //old = redMat;
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(redMat, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        double maxArea = -1;
+        int maxAreaIdx = -1;
+        Rect boundingRect = new Rect();
+        for (int idx = 0; idx < contours.size(); idx++) {
+            boundingRect = Imgproc.boundingRect(contours.get(idx));
+
+            Mat contour = contours.get(idx);
+            double contourarea = Imgproc.contourArea(contour);
+            if (contourarea > maxArea) {
+                maxArea = contourarea;
+                maxAreaIdx = idx;
+            }
+        }
+
+        // This is the largest contour
+        if(maxAreaIdx != -1 && boundingRect != null)
+        {
+            // Print the area
+            Log.w(TAG, "area = " + maxArea);
+            Imgproc.rectangle(redMat, boundingRect.tl(), boundingRect.br(), new Scalar(255, 0, 0), 1);
+        }
+
+        return redMat;
     }
 }
